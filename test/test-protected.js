6023d5872a3c23e9135cdc9f9466fe8df11fb8db
Lavanya S <lavanya.sethuraman23@gmail.com>
2017-09-27 15:24:16 -0400

    Date Picker

--- test/test-protected.js
+++ test/test-protected.js
@@ -117,7 +117,7 @@ describe('Protected endpoint', function() {
         .then(res => {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('object');
-          expect(res.body.data).to.equal('rosebud');
+          expect(res.body.data).to.an('object');
         });
     });
   });
